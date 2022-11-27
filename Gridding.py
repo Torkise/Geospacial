1f2a71984583a1c1b932dd8735292e3907046ded
Torkil Seljestokken <Torkil96@gmail.com>
2022-11-27 18:27:07 +0100

    progga litt

--- Gridding.py
+++ Gridding.py
@@ -8,35 +8,50 @@ f_obs = [1, 3, 2, 1]
 
 def weight(d):
     if d <= 1:
-        return math.e**(math.log(0.1, math.e)*d**2)
+        return (1/10)/((1/10)+d**2)
     else:
         return 0 
     
 def distance(x_0, x_1, y_0, y_1):
     return math.sqrt((x_1-x_0)**2 + (y_1-y_0)**2)
-    
-grid_notes = [[0, 0], [1, 1], [2, 2]]
-grid_weights = []
-grid_f = np.zeros(len(grid_notes))
 
-for node in grid_notes:
+def grid_var(variance, weights):
+    if(sum(weights) == 0): return 0
+    return (variance*sum([w**2 for w in weights ]))/(sum(weights)**2)
+    
+grid_nodes = [[0, 0], [1, 1], [2, 2]]
+grid_weights = []
+grid_dist = []
+grid_f = np.zeros(len(grid_nodes))
+
+for node in grid_nodes:
     f = 0
     w = 0
     grid_weights.append([])
+    grid_dist.append([])
     for i in range(len(x_obs)):
         dist = distance(node[0], x_obs[i], node[1], y_obs[i])
         w_i = weight(dist)
-        grid_weights[grid_notes.index(node)].append(w_i)
+        grid_dist[grid_nodes.index(node)].append(dist)
+        grid_weights[grid_nodes.index(node)].append(w_i)
         if w_i > 0:
             f = f + f_obs[i]*w_i
             w = w + w_i
     
-    if w == 0: grid_f[grid_notes.index(node)] = 0
-    else: grid_f[grid_notes.index(node)] = f/w
+    if w == 0: grid_f[grid_nodes.index(node)] = 0
+    else: grid_f[grid_nodes.index(node)] = f/w
     
 
 
-print("Grid Notes\n", grid_notes)
-print("Grid Node Values \n",grid_f)
-print("Grid Weights\n",grid_weights)
+print("Grid Notes\n", grid_nodes)
+for i in range(len(grid_nodes)):
+    print("Grid Node", grid_nodes[i])
+    print("Distances \n", grid_dist[i])
+    print("Weights: ", grid_weights[i])
+    print("F_val", grid_f)
+    
+#Define Variance 
+variance = 0.01
+for i in range(len(grid_nodes)):
+    print("Var for Node", grid_nodes[i], ": ",grid_var(variance, grid_weights[i]))
     
--- cubicsplineLSM.py
+++ cubicsplineLSM.py
@@ -1,6 +1,6 @@
 import numpy as np
-x_obs = [0.2, 0.9, 1, 2]
-y_obs = [1, 2, -1, -4]
+x_obs = [0.0, 1.0, 1.5, 2]
+y_obs = [2.5, 2, -1, -2]
 k_val = [0, 1, 2]
 
 def sigma(x, k):
@@ -18,7 +18,8 @@ for x in range (len(x_obs)):
 
 N = A.T@A
 a_hat = np.linalg.inv(N)@A.T@y_obs
-
+print("x_obs", x_obs)
+print("y obs", y_obs)
 print("A - matrice ####### \n", A, "\n N matrice = A.T dot A ########\n",N, "\n a hat #####\n",a_hat)
     
 def f(x, a_hat, k): 
@@ -35,6 +36,15 @@ v = np.array([y_obs[i] - f_hat[i] for i in range(len(y_obs))])
 print("v  #############\n", v)
 
 var_0 = (v.T@v)/(len(x_obs)-len(k_val))
-var_all = [var_0 * np.linalg.inv(N)[i][i] for i in range(len(k_val))]
+var_all = var_0 * np.linalg.inv(N)
 print(var_0)
 print(var_all)
+D = np.array([[-1, 1, 0],[0, -1, 1]])
+print(D)
+
+N_D = D.T@D
+lam = 0.1
+N_T = N + lam*N_D
+print("N_T = \n", N, "\n+ \n ",lam, "\n*\n", N_D  )
+
+print("N_T = \n", N_T)
